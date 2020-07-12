6e4dcbd1db5becb63b4b063948f60cab98907e6b
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-07-12 13:51:42 -0300

    feat: add image and user name

--- index.php
+++ index.php
@@ -26,7 +26,23 @@ $array_com_os_valores = pg_fetch_all($ponteiro);
 
         h1 {
             background: #999;
-            padding: 20px;
+            padding: 10px 20px;
+            font-size: 18px;
+            color: white;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+        h1 img {
+            border-radius: 50%;
+            border: 1px solid #333;
+            width: 30px;
+            height: 30px;
+        }
+
+        #lista {
+            max-width: 400px;
         }
 
         ul {
@@ -45,26 +61,41 @@ $array_com_os_valores = pg_fetch_all($ponteiro);
             background-color: #FFD;
         }
 
-        #lista {
-            max-width: 400px;
-        }
-        li a{
+        li a {
             display: inline-block;
             text-align: center;
             color: red;
             width: 18px;
             text-decoration: none;
         }
-        li a:hover{
-            background: rgba(0,0,0,0.2);
+
+        li a:hover {
+            background: rgba(0, 0, 0, 0.2);
         }
-        form{
+
+        li > div {
+            display: inline-flex;
+            align-items: center;
+        }
+
+        li .item-and-user-name {
+            margin-left: 5px;
+        }
+
+        li .user-name {
+            color: #DDD;
+            font-size: 10px;
+        }
+
+        form {
             border: 1px solid #888;
             padding: 10px;
         }
+
         label {
             display: block;
         }
+
         input {
             width: 100%;
             border: 1px solid #eaeaea;
@@ -73,18 +104,28 @@ $array_com_os_valores = pg_fetch_all($ponteiro);
             margin-bottom: 10px;
             padding: 5px;
         }
-        h2{
+
+        h2 {
             margin-top: 0;
         }
     </style>
 </head>
 <body>
 <div id="lista">
-    <h1>Lista de compras</h1>
+    <h1>
+        Lista de compras
+        <img src="https://picsum.photos/30/30"/>
+    </h1>
     <ul>
         <?php foreach ($array_com_os_valores as $item): ?>
             <li>
-                <span><?= $item['quantity'] ?> - <?= $item['name'] ?></span>
+                <div>
+                    <div><?= $item['quantity'] ?> -</div>
+                    <div class="item-and-user-name">
+                        <div><?= $item['name'] ?></div>
+                        <div class="user-name">Nome de quem criou</div>
+                    </div>
+                </div>
                 <a href="#">X</a>
             </li>
         <?php endforeach ?>
@@ -93,11 +134,11 @@ $array_com_os_valores = pg_fetch_all($ponteiro);
         <h2>Adicionar novo ítem</h2>
         <div>
             <label>Nome do ítem:</label>
-            <input name="name" placeholder="exemplo: batata" />
+            <input name="name" placeholder="exemplo: batata"/>
         </div>
         <div>
             <label>Quantidade:</label>
-            <input name="quantity" placeholder="exemplo: 5" type="number" />
+            <input name="quantity" placeholder="exemplo: 5" type="number"/>
         </div>
         <div>
             <button>Adicionar</button>
