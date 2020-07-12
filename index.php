0a5b2037f37eecea7562994c33e959f4c519ae2b
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-07-11 23:31:48 -0300

    feat: update style

--- index.php
+++ index.php
@@ -58,6 +58,24 @@ $array_com_os_valores = pg_fetch_all($ponteiro);
         li a:hover{
             background: rgba(0,0,0,0.2);
         }
+        form{
+            border: 1px solid #888;
+            padding: 10px;
+        }
+        label {
+            display: block;
+        }
+        input {
+            width: 100%;
+            border: 1px solid #eaeaea;
+            border-bottom: 1px solid #333;
+            border-radius: 0;
+            margin-bottom: 10px;
+            padding: 5px;
+        }
+        h2{
+            margin-top: 0;
+        }
     </style>
 </head>
 <body>
@@ -71,6 +89,20 @@ $array_com_os_valores = pg_fetch_all($ponteiro);
             </li>
         <?php endforeach ?>
     </ul>
+    <form action="/add.php" method="post">
+        <h2>Adicionar novo ítem</h2>
+        <div>
+            <label>Nome do ítem:</label>
+            <input name="name" placeholder="exemplo: batata" />
+        </div>
+        <div>
+            <label>Quantidade:</label>
+            <input name="quantity" placeholder="exemplo: 5" type="number" />
+        </div>
+        <div>
+            <button>Adicionar</button>
+        </div>
+    </form>
 </div>
 </body>
 </html>
