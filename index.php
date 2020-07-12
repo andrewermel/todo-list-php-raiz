2183d84ae46da4d02d04ce14486b290774364e49
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-07-11 22:20:10 -0300

    feat: add style

--- index.php
+++ index.php
@@ -14,17 +14,64 @@ $array_com_os_valores = pg_fetch_all($ponteiro);
 
 <html>
 <head>
-	<title>Do To list do Andrew</title>
+    <title>Do To list do Andrew</title>
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        h1 {
+            background: #999;
+            padding: 20px;
+        }
+
+        ul {
+            padding: 0;
+            list-style: none;
+        }
+
+        li {
+            display: flex;
+            padding: 10px;
+            border-bottom: 1px solid #666;
+            justify-content: space-between;
+        }
+
+        li:hover {
+            background-color: #FFD;
+        }
+
+        #lista {
+            max-width: 400px;
+        }
+        li a{
+            display: inline-block;
+            text-align: center;
+            color: red;
+            width: 18px;
+            text-decoration: none;
+        }
+        li a:hover{
+            background: rgba(0,0,0,0.2);
+        }
+    </style>
 </head>
 <body>
-	<h1>Lista de compras</h1>
-	<ul>
-		<?php foreach($array_com_os_valores as $item): ?>
-			<li>
-				<?= $item['quantity']?> - <?= $item['name']?>
-			</li>
-		<?php endforeach ?>
-	</ul>
+<div id="lista">
+    <h1>Lista de compras</h1>
+    <ul>
+        <?php foreach ($array_com_os_valores as $item): ?>
+            <li>
+                <span><?= $item['quantity'] ?> - <?= $item['name'] ?></span>
+                <a href="#">X</a>
+            </li>
+        <?php endforeach ?>
+    </ul>
+</div>
 </body>
 </html>
 
