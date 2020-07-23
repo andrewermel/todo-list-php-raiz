869930504f3874c92a872d9134e6e429da61f077
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-07-23 10:39:35 -0300

    Add validação de nome (vazio e já existente)

--- add.php
+++ add.php
@@ -4,6 +4,10 @@ require 'database.php';
 $quantity = sql_anti_injection($_POST['quantity']);
 $name = sql_anti_injection($_POST['name']);
 
+if(!$name) {
+    die("Não foi possível cadastras um ítem sem nome");
+}
+
 if(!$quantity){
   $query = "INSERT INTO lista (name) VALUES ('$name')";
 }
@@ -11,6 +15,12 @@ else{
   $query = "INSERT INTO lista (quantity, name) VALUES ($quantity, '$name')";
 }
 
-$ponteiro = db_execute_query($query);
+$ponteiro = db_execute_query("SELECT * FROM lista WHERE name = '$name'");
+$item = pg_fetch_assoc($ponteiro);
 
-header('Location:  /');
+if(!$item) {
+  db_execute_query($query);
+  header('Location:  /');
+}
+
+die("Não foi possível cadastras um ítem com nome $name");
