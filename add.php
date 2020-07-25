3f79ee7e3a9ebbab78e8fe6c4c4c2a7800f9d6b0
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-07-25 17:36:53 -0300

    Novo banco

--- add.php
+++ add.php
@@ -2,11 +2,11 @@
 
 
 
-$host='ec2-3-208-50-226.compute-1.amazonaws.com';
+$host='ec2-34-225-162-157.compute-1.amazonaws.com';
 $port='5432';
-$database='d7r0ukrhem4kv0';
-$user='shqfnemscchnnh';
-$pass='a3dc6f7ca7fc5c83f8ab3109d05c24fb3028d3ea663f7cd4947cb3212dc08485';
+$database='dffirnq6pc5iim';
+$user='bbmhndayvriejv';
+$pass='0eb27693698aa48ed1de42b09c88d0243774cd52d22020c4af05d5163bd98f32';
 
 
 $conexao= pg_connect("host=$host port=$port dbname=$database user=$user password=$pass");
