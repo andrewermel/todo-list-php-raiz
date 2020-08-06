eff2155a8be4cdecc080e618124c7eebb0cddd48
Andre Antunes Vieira <andreantunesv@gmail.com>
2020-08-06 12:27:02 -0300

    trocando variaveis do banco

--- add.php
+++ add.php
@@ -1,17 +1,13 @@
 <?php
 
-
-
-$host='ec2-34-225-162-157.compute-1.amazonaws.com';
+$host='ec2-54-160-120-28.compute-1.amazonaws.com';
 $port='5432';
-$database='dffirnq6pc5iim';
-$user='bbmhndayvriejv';
-$pass='0eb27693698aa48ed1de42b09c88d0243774cd52d22020c4af05d5163bd98f32';
+$database='db5relmq3c860s';
+$user='rnrwrasadzshjd';
+$pass='1328e0d0763daa678213de11c28f4f63266617b41448ca9c7023c92a936ce91f';
 
 
 $conexao= pg_connect("host=$host port=$port dbname=$database user=$user password=$pass");
-//$conexao = new PDO("pgsql:host=$host;port=$port;dbname=$database",$user,$pass);
-
 
 
 $quantidade = $_POST['quantidade'];
